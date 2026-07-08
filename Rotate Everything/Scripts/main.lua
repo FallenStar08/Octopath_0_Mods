bbb5aa95b75bade8c5eadd952c8a52440caba1c1
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-07-08 09:19:19 +0200

    rotate everythiiiing

---
+++ Rotate Everything/Scripts/main.lua
@@ -0,0 +1,21 @@
+local UEHelpers = require("UEHelpers")
+
+
+local sd = debug.getinfo(1, 'S').source:sub(2):match("^(.*[\\/])") or ""
+--Add the current mod directory and the 'Shared' subfolder to the search path
+package.path = sd .. "/?.lua;" .. sd .. "/Shared/?.lua;" .. package.path
+
+local H = require("Shared.Helpers")
+local OH = require("Shared.OctoHelpers")
+H.Init("Rotate Everything :")
+
+
+RegisterHook("/Script/Engine.PlayerController:ClientRestart", function()
+    OH.Patch("m_VillageBuildingData.m_DataList")
+        :All()
+        :Cooldown(5000)
+        :Delay(2000)
+        :Execute(function(entry)
+            entry.m_RotationType = 2
+        end)
+end)
