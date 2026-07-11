2b9064daf4c40198b827e9a94a33b5b9ecb1f950
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2026-07-11 12:05:04 +0200

    use cache for live edits

--- CustomDifficulty/Scripts/main.lua
+++ CustomDifficulty/Scripts/main.lua
@@ -8,37 +8,45 @@ package.path = sd .. "/?.lua;" .. sd .. "/Shared/?.lua;" .. package.path
 local H = require("Shared.Helpers")
 local OH = require("Shared.OctoHelpers")
 local config = require("config")
-H.Init("CustomDifficulty :")
+H.Init("Custom Difficulty :")
+
+
+local OriginalStatsCache = {}
 
 local function patch()
-    if not config.GlobalBoost.BoostNormals and not config.GlobalBoost.BoostBosses then
-        H.LogDebug("Both BoostNormals and BoostBosses are disabled. No modifications will be applied.")
-        return
-    end
     OH.Patch("m_EnemyID.m_DataList")
         :All()
-        :Cooldown(5000)
-        :Delay(2000)
         :Execute(function(entry)
-            entry.m_HpUI = true
-            local isBoss = entry.m_IsBoss
-            if config.GlobalBoost.BoostBosses == false and isBoss then
-                return
-            end
-            if config.GlobalBoost.BoostNormals == false and not isBoss then
-                return
-            end
-            local modifiers = (isBoss and config.GlobalBoost.UseBossSpecificBoost and config.GlobalBoost.Boss) or
-                config.GlobalBoost.Normal
+            local id = entry.m_id
 
-            for field, multiplier in pairs(modifiers) do
-                if entry[field] ~= nil and type(entry[field]) == "number" then
-                    entry[field] = math.floor(entry[field] * multiplier)
+            if not OriginalStatsCache[id] then
+                OriginalStatsCache[id] = {}
+                local modifiers = config.GlobalBoost.Normal
+                for field, _ in pairs(modifiers) do
+                    if entry[field] ~= nil then
+                        OriginalStatsCache[id][field] = entry[field]
+                    end
                 end
             end
+
+            local isBoss = entry.m_IsBoss
+            if (isBoss and not config.GlobalBoost.BoostBosses) or
+                (not isBoss and not config.GlobalBoost.BoostNormals) then
+                return
+            end
+
+            local modifiers = (isBoss and config.GlobalBoost.UseBossSpecificBoost and config.GlobalBoost.Boss)
+                or config.GlobalBoost.Normal
+
+            for field, multiplier in pairs(modifiers) do
+                if OriginalStatsCache[id][field] ~= nil then
+                    entry[field] = math.floor(OriginalStatsCache[id][field] * multiplier)
+                end
+            end
+
+            entry.m_HpUI = true
         end)
 end
-
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", patch)
 
 RegisterKeyBind(Key.F5, patch)
